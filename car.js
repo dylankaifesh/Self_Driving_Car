47e63de40a414068e0a4d056cd28c314631991a7
MoistToast24 <kaifeshd@gmail.com>
2023-07-20 15:41:10 -0500

    Car movements

--- car.js
+++ car.js
@@ -15,6 +15,10 @@ class Car{
     }
 
     update(){
+        this.#move();
+    }
+
+    #move(){
         if(this.controls.forward){
             this.speed+=this.acceleration;
         }
@@ -39,14 +43,17 @@ class Car{
             this.speed=0;
         }
 
-        if(this.controls.left){
-            this.angle+=0.03; //uses the unit circle
+        if(this.speed!=0){
+            const flip=this.speed>0?1:-1;
+            if(this.controls.left){
+                this.angle+=0.03*flip; //uses the unit circle
+            }
+            if(this.controls.right){
+                this.angle-=0.03*flip;//uses the unit circle
+            }
         }
-        if(this.controls.right){
-            this.angle-=0.03
-        }
-
-        this.y-=this.speed;
+        this.x-=Math.sin(this.angle)*this.speed; //uses unit circle
+        this.y-=Math.cos(this.angle)*this.speed; //uses unit circle
     }
 
     draw(ctx){
