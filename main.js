ef3e4eb07e1f79f39e851f17160e409af40d770e
dylankaifesh <kaifeshd@gmail.com>
2023-08-01 08:56:10 -0500

    algorithm

--- main.js
+++ main.js
@@ -11,19 +11,31 @@ const N = 100;
 const cars=generateCars(N);
 let bestCar=cars[0];
 if(localStorage.getItem("bestBrain")){
-    bestCar.brain=JSON.parse(
-        localStorage.getItem("bestBrain"));
+    for(let i=0;i<cars.length;i++){
+        cars[i].brain=JSON.parse(
+            localStorage.getItem("bestBrain"));
+        if(i!=0){
+            NeuralNetwork.mutate(cars[i].brain,0.2);
+        }
+    }
+    
 }
 
 const traffic=[
-    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2)
+    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-700,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-700,30,50,"DUMMY",2),
 ];
 
 animate();
 
 function save(){
     localStorage.setItem("bestBrain",
-    JSON.stringify(bestCar.brain));
+        JSON.stringify(bestCar.brain));
 }
 
 function discard(){
@@ -47,7 +59,7 @@ function animate(time){
         cars[i].update(road.borders,traffic);
     }
 
-    const bestCar=cars.find(
+    bestCar=cars.find(
         c=>c.y==Math.min(
             ...cars.map(c=>c.y)
         ));
@@ -76,4 +88,4 @@ function animate(time){
     requestAnimationFrame(animate);
 }
 
-//2:08:17
+//2:22:51
